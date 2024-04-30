4dfbcf31935fb5f78f026fd520480994f8ba3b13
alisunnyday <calpurniahepburn@gmail.com>
2024-04-29 19:29:15 -0700

    call me a derivative the way I'm at my limit 4/29

--- src/BlockyAnimal.js
+++ src/BlockyAnimal.js
@@ -101,19 +101,6 @@ function addActionsforHTMLUI() {
 
     document.getElementById('animationMagentaOffButton').onclick = function() { g_magentaAnimation = false; };
     document.getElementById('animationMagentaOnButton').onclick = function() { g_magentaAnimation = true; };
-    
-    // Clear
-    document.getElementById('clear').onclick = function() { g_shapesList = []; renderAllShapes() };
-
-    // Shapes
-    document.getElementById('point').onclick = function() { g_selectedType = POINT };
-    document.getElementById('triangle').onclick = function() { g_selectedType = TRIANGLE };
-    document.getElementById('circle').onclick = function() { g_selectedType = CIRCLE };
-
-    // Color slider events
-    // document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
-    // document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
-    // document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
 
     // Size slider
     document.getElementById('rotateYellow').addEventListener('mousemove', function () { g_yellowAngle = this.value; renderAllShapes() });
@@ -137,103 +124,6 @@ function main() {
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    // renderAllShapes();
-    // requestAnimationFrame(tick);
-}
-
-// var g_startTime = performance.now()/1000;
-// var g_seconds = performance.now()/1000 - g_startTime;
-
-// function tick() {
-//     g_seconds = (performance.now()/1000) - g_startTime;
-//     updateAnimationAngles();
-//     console.log(g_seconds);
-//     renderAllShapes();
-//     requestAnimationFrame(tick);
-// }
-
-function updateAnimationAngles() {
-    if (g_yellowAnimation) {
-        g_yellowAngle = (45 * Math.sin(g_seconds));
-    }
-
-    if (g_magentaAnimation) {
-        g_magentaAngle = (45 * Math.sin(3 * g_seconds));
-      }
-
-}
-
-function renderAllShapes() {
-    var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
-    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Colors
-
-
-    // Bunny head
-
-    var head = new Cube();
-    head.color = [1, 1, 0, 1];
-    head.matrix.scale(0.4, 0.4, 0.4);
-    head.matrix.translate(-0.5, 0, -2.125);
-    // head.matrix.rotate(-g_yellowAngle, 1, 1, 0);
-    head.render();
-
-    // Ears
-    var ear1 = new Cube();
-    ear1.color = [1, 1, 0, 1];
-    ear1.matrix.scale(0.1, 0.6, 0.1);
-    ear1.matrix.translate(-1.5, 0.25, -5.5);
-    // rotate slightly
-    ear1.render();
-
-    var ear2 = new Cube();
-    ear2.color = [1, 1, 0, 1];
-    ear2.matrix.scale(0.1, 0.6, 0.1);
-    ear2.matrix.translate(0.5, 0.25, -5.5);
-    // rotate slightly
-    ear2.render();
-
-    
-    // Body
-
-    var body = new Cube();
-    body.color = [1, 1, 0, 1];
-    body.matrix.scale(0.5, 0.4, 0.8);
-    body.matrix.translate(-0.5, -1, -1);
-    body.matrix.rotate(20, 1, 0, 0);
-    // body.matrix.setTranslate(0, -0.5, 0.0);
-    // body.matrix.rotate(-5, 1, 0, 0);
-    // body.matrix.rotate(-g_yellowAngle, 0, 0, 1);
-    // body.matrix.rotate(45*Math.sin(g_seconds), 0, 0, 1);
-    // var yellowCoordinatesMat = new Matrix4(body.matrix);
-    // body.matrix.scale(0.25, 0.7, 0.5);
-    // body.matrix.translate(-0.5, 0, 0);
-    body.render();
-
-
-    
-    // var notbody = new Cube();
-    // notbody.color = [1.0, 0.0, 0.0, 1.0];
-    // notbody.matrix.translate(-0.25, -0.75, 0.0);
-    // notbody.matrix.rotate(-5, 1, 0, 0);
-    // notbody.matrix.scale(0.5, 0.3, 0.5);
-    // notbody.render();
-
-    
-
-    // Test cube
-    // var magenta = new Cube();
-    // magenta.color = [1,0,1,1];
-    // magenta.matrix = yellowCoordinatesMat;
-    // magenta.matrix.translate(0, 0.65, 0);
-    // magenta.matrix.rotate(-g_magentaAngle,0,0,1);
-    // magenta.matrix.scale(0.3, 0.3, 0.3);
-    // magenta.matrix.translate(-0.5,0, -0.001);
-    // magenta.render();
 }
 
 var g_shapesList = [];
@@ -271,3 +161,139 @@ function convertCoordinatesEventToGL(ev) {
 
     return([x, y]);
 }
+
+function updateAnimationAngles() {
+    if (g_yellowAnimation) {
+        g_yellowAngle = (45 * Math.sin(g_seconds));
+    }
+
+    if (g_magentaAnimation) {
+        g_magentaAngle = (45 * Math.sin(3 * g_seconds));
+      }
+
+}
+
+function renderAllShapes() {
+    var startTime = performance.now();
+
+    var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Colors
+    var PINK = [0.830, 0.681, 0.681, 1.0];
+
+
+    // Bunny head
+
+    var head = new Cube();
+    head.color = [1, 1, 0, 1];
+    head.matrix.scale(0.45, 0.4, 0.4);
+    head.matrix.translate(-0.5, -0.1, -2.125);
+    // head.matrix.rotate(-g_yellowAngle, 1, 1, 0);
+    head.render();
+
+    // Ears
+    var ear1 = new Cube();
+    ear1.color = [1, 1, 0, 1];
+    ear1.matrix.scale(0.11, 0.6, 0.1);
+    ear1.matrix.translate(-1.5, 0.25, -5.75);
+    ear1.matrix.rotate(-20, 1, 45, 0);
+    ear1.render();
+
+    var ear2 = new Cube();
+    ear2.color = [1, 1, 0, 1];
+    ear2.matrix.scale(0.11, 0.6, 0.1);
+    ear2.matrix.translate(0.5, 0.25, -5.75);
+    ear2.matrix.rotate(20, 1, 45, 0);
+    ear2.render();
+
+    var nose = new Cube();
+    nose.color = PINK;
+    nose.matrix.scale(0.1, 0.1, 0.1);
+    nose.matrix.translate(-0.5, 1.5, -9);
+    nose.render();
+
+    
+    // Body
+
+    var body = new Cube();
+    body.color = [1, 1, 0, 1];
+    body.matrix.scale(0.5, 0.4, 0.8);
+    body.matrix.translate(-0.5, -0.7, -0.8);
+    body.matrix.rotate(12, 1, 0, 0);                    // why is it so pointy
+    // body.matrix.setTranslate(0, -0.5, 0.0);
+    // body.matrix.rotate(-5, 1, 0, 0);
+    // body.matrix.rotate(-g_yellowAngle, 0, 0, 1);
+    // body.matrix.rotate(45*Math.sin(g_seconds), 0, 0, 1);
+    // var yellowCoordinatesMat = new Matrix4(body.matrix);
+    // body.matrix.scale(0.25, 0.7, 0.5);
+    // body.matrix.translate(-0.5, 0, 0);
+    body.render();
+
+    // Back haunches
+    var haunchL = new Cube();
+    haunchL.matrix.scale(0.13, 0.27, 0.3);
+    haunchL.matrix.translate(-2, -2.5, 0);
+    // haunchL.matrix.rotate();
+    haunchL.render();
+
+    var haunchR = new Cube();
+
+    // Legs
+    var frontlegL = new Cube();
+    frontlegL.color = PINK;
+    frontlegL.matrix.scale(0.13, 0.5, 0.13);
+    frontlegL.matrix.translate(-2, -1.4, -5); 
+    frontlegL.matrix.rotate(10, 0, 1, 0);               // tilt legs forward?
+    frontlegL.render();
+
+    var frontlegR = new Cube();
+    frontlegR.color = PINK;
+    frontlegR.matrix.scale(0.13, 0.5, 0.13);
+    frontlegR.matrix.translate(1, -1.4, -5); 
+    frontlegR.matrix.rotate(10, 0, 1, 0);
+    frontlegR.render();
+
+
+    var backlegL = new Cube();
+
+    var backlegR = new Cube();
+
+    var tail = new Cube();
+    
+
+    
+    // var notbody = new Cube();
+    // notbody.color = [1.0, 0.0, 0.0, 1.0];
+    // notbody.matrix.translate(-0.25, -0.75, 0.0);
+    // notbody.matrix.rotate(-5, 1, 0, 0);
+    // notbody.matrix.scale(0.5, 0.3, 0.5);
+    // notbody.render();
+
+    
+
+    // Test cube
+    // var magenta = new Cube();
+    // magenta.color = [1,0,1,1];
+    // magenta.matrix = yellowCoordinatesMat;
+    // magenta.matrix.translate(0, 0.65, 0);
+    // magenta.matrix.rotate(-g_magentaAngle,0,0,1);
+    // magenta.matrix.scale(0.3, 0.3, 0.3);
+    // magenta.matrix.translate(-0.5,0, -0.001);
+    // magenta.render();
+
+    var duration = performance.now() - startTime;
+	sendTextToHTML(" ms: " + Math.floor(duration) + " fps: " + Math.floor(10000/duration)/10, "numdot");
+}
+
+function sendTextToHTML(text, htmlID) {   // we take the text and its htmlID
+    var htmlElm = document.getElementById(htmlID);
+    if (!htmlElm) {
+      console.log("Failed to get " + htmlID + " from HTML");
+      return;
+    }
+    htmlElm.innerHTML = text; // send inner html to whatver the text was
+  }
